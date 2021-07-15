b5b884221f2087edba2fb180e684ea937e98ed48
codebender828 <jbakebwa@163.com>
2021-07-15 17:09:30 +0800

    feat: add client only fix for ssg

--- src/components/c-mobile-nav.vue
+++ src/components/c-mobile-nav.vue
@@ -1,16 +1,18 @@
 <template>
-  <c-box>
-    <c-icon-button bg="none" position="absolute" top="4" left="4" :icon="isOpen ? 'x' : 'menu'" :aria-label="`${isOpen ? 'Close' : 'Open'} nav menu`" @click="isOpen = !isOpen" />
-    <c-drawer v-model="isOpen" size="full" placement="left">
-      <c-drawer-overlay />
-      <c-drawer-content>
-        <c-drawer-body pt="5">
-          <c-drawer-close-button />
-          <c-side-nav bg="transparent" />
-        </c-drawer-body>
-      </c-drawer-content>
-    </c-drawer>
-  </c-box>
+  <client-only>
+    <c-box :display="{ base: 'flex', sm: 'flex', md: 'none' }">
+      <c-icon-button bg="none" position="absolute" top="4" left="4" :icon="isOpen ? 'x' : 'menu'" :aria-label="`${isOpen ? 'Close' : 'Open'} nav menu`" @click="isOpen = !isOpen" />
+      <c-drawer v-model="isOpen" size="full" placement="left">
+        <c-drawer-overlay />
+        <c-drawer-content>
+          <c-drawer-body pt="5">
+            <c-drawer-close-button />
+            <c-side-nav bg="transparent" />
+          </c-drawer-body>
+        </c-drawer-content>
+      </c-drawer>
+    </c-box>
+  </client-only>
 </template>
 
 <script setup lang="ts">
--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -3,7 +3,7 @@
   <c-grid :template-columns="{ base: '1fr', sm: '1fr', md: '250px 1fr' }" h="100vh" :pt="{ base: 10, sm: 10, md: 0 }" w="100vw">
     <c-side-nav :grid-column="{ base: '1/2', sm: '1/2', md: '1/2'}" :d="['none', 'none', 'flex']" />
     <chakra.main :grid-column="{ base: '1/2', sm: '1/2', md: '2/3'}">
-      <c-mobile-nav :d="['flex', 'flex', 'none']" />
+      <c-mobile-nav />
       <router-view />
     </chakra.main>
   </c-grid>
